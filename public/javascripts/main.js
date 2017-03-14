825e95220906a179c13e2a194198924bd19f600a
Jack Deadman <jdeadman1@sheffield.ac.uk>
2017-03-14 18:31:27 +0000

    Front end talking to backend through sockets

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,22 @@
-alert('loaded');
+function handleSearchError(err) {
+  // TODO: Add error handling
+}
+
+function displaySearchResults(results) {
+  // TODO: Do fancy stuff here
+  if (results.tweets.length) {
+      alert(results.tweets[0].text)
+  }
+}
+
+
+(function(socket) {
+  // Setup Socket listeners
+  socket.on('searchError', handleSearchError);
+  socket.on('searchResult', displaySearchResults);
+
+  // Send some data to the server to test
+  var req = { player: 'Wayne', club: 'Brighton' };
+  socket.emit('search', req);
+
+})(io());
--- sockets/index.js
+++ sockets/index.js
@@ -1,11 +1,27 @@
+function findTransfers(player, club) {
+  var results = {
+    tweets: [
+      { text: 'OMG WAYNE ROONEY IS GOING 2 BRIGHTON' }
+    ]
+  };
+  return results;
+}
+
 module.exports = {
   listen: function(server) {
     var io = require('socket.io')(server);
 
     io.on('connection', function(socket) {
       console.log('connected.');
-      socket.on('search', function(socket) {
-        
+      socket.on('search', function(req) {
+        if (req && req.player && req.club) {
+          var results = findTransfers(req.player && req.club);
+          socket.emit('searchResult', results);
+        } else {
+          socket.emit('searchError', {
+            errorMessage: 'Please provide a player and a club name.'
+          });
+        }
       });
     });
   }
