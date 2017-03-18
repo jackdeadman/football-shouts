26caf3ca2b747f544e07ed4e55ae25bfc899d372
George Baron <gbaron1@sheffield.ac.uk>
2017-03-18 17:52:59 +0000

    Tidied up slightly and added some default tags for testing purposes

---
+++ public/javascripts/init.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('#players').materialtags('add', 'Wayne Rooney');
+  $('#players').materialtags('add', '@waynerooney');
+  $('#clubs').materialtags('add', 'Brighton');
+  $('#clubs').materialtags('add', '@brighton');
+});
