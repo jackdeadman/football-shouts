a9e1663ddab1f6bf76619af1c41816bb50e52d10
George Baron <gbaron1@sheffield.ac.uk>
2017-03-30 23:32:54 +0100

    Small fix regarding link to twitter page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,7 +29,7 @@ function createTweetNode(tweet) {
                 .prepend('<div class = "tweetDate">' + moment(tweet.datePublished).format('LLL') + '</div>')
                 .prepend('<span class = "black-text">' + tweetText + '</span>')
                 .prepend('<div class="tweetTop"><div class="tweetName">' +
-                         '<a href = "https://twitter.com/' + tweet.handle + '" target = "_blank" class = "black-text">' + tweet.name + '</a>' +
+                         '<a href = "https://twitter.com/' + tweet.twitterHandle + '" target = "_blank" class = "black-text">' + tweet.name + '</a>' +
                          '</div><div class="tweetHandle"> ' +
                          '<a href = "https://twitter.com/' + tweet.twitterHandle + '" target = "_blank">@' + tweet.twitterHandle + '</a></div></div>');
 
