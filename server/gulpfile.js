fb5155171f0eb11df5771dfaff996182a8c836a5
Simon Turner <simonturner34@gmail.com>
2017-05-18 23:34:08 +0100

    Adds deploy task to gulpfile

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,6 +2,10 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var argv = require('yargs')
+            .usage('Usage: $0 -ios to run ios app, -android to run android app.')
+            .argv;
+var exec = require('exec-chainable');
 
 gulp.task('lint', function () {
   var files = [
@@ -13,4 +17,32 @@ gulp.task('lint', function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('deploy', ['copy-jade', 'copy-public'], function () {
+  if (argv.ios) {
+    exec('cordova run ios');
+  } else if (argv.android) {
+    exec('cordova run android');
+  }
+});
+
+gulp.task('copy-public', () => {
+  var files = [
+    'webapp/public/**/**/*',
+  ];
+  var outputPath = 'mobile/FootballShouts/www/';
+  return gulp
+    .src(files)
+    .dest(outputPath);
+});
+
+gulp.task('copy-jade', () => {
+  var jadeFiles = [
+    'webapp/views/index.jade'
+  ];
+  var outputPath = 'mobile/FootballShouts/jade';
+  return gulp
+    .src(jadeFiles)
+    .dest(outputPath);
 });
